f201311e008618a44bb3da7da9d9d8015e106f5f
krishamehta <kkoolkrisha@gmail.com>
2017-07-01 13:02:13 +0530

    Jupyter file added

--- beer.py
+++ beer.py
@@ -3,3 +3,45 @@ from bs4 import BeautifulSoup
 import pandas as pd 
 import re
 
+
+def is_beer_entry(table_row):
+	row_cells = table_row.findAll("td")
+	beer_id = get_beer_id(row_cells[0].text)
+	return (len(row_cells)==8 and beer_id)
+
+def get_beer_id(cell_value):
+	r = re.match("^(\d{1,4})\.$", cell_value)
+	if r and len(r.groups())==1:
+		beer_id = r.group(1)
+		return int(beer_id)
+	else:
+		return None	
+
+
+
+def get_all_beers(html_soup):
+    beers = []
+    all_rows_in_html_page = html_soup.findAll("tr")
+    for table_row in all_rows_in_html_page:
+        if is_beer_entry(table_row):
+            row_cells = table_row.findAll("td")
+            beer_entry = {
+                "id": get_beer_id(row_cells[0].text),
+                "name": row_cells[1].text,
+                "brewery_name": row_cells[2].text,
+                "brewery_location": row_cells[3].text,
+                "style": row_cells[4].text,
+                "size": row_cells[5].text,
+                "abv": row_cells[6].text,    
+                "ibu": row_cells[7].text
+            }
+            beers.append(beer_entry)
+    return beers
+
+html = urlopen("http://craftcans.com/db.php?search=all&sort=beerid&ord=desc&view=text")
+html_soup = BeautifulSoup(html, 'html.parser')
+beers_list = get_all_beers(html_soup)
+
+df = pd.DataFrame(beers_list)
+df.head(5)
+df
--- send_sms.py
+++ send_sms.py
@@ -2,14 +2,14 @@ import os
 
 from twilio.rest import Client
 
-account_sid = "AC296b29fe20967a9d0b5c799b9cc55a47"
-auth_token = "c315683ba3d453c8907c09a4be1a9bc9"
+account_sid = ""
+auth_token = ""
 
 client = Client(account_sid,auth_token)
 
 client.messages.create(
 
-	to ="+919930087431" ,
-	from_ = "+12513330911",
+	to ="" ,
+	from_ = "",
 	body="Ground control to Major Tom"
 	)
